623bbe4ea458184760ee4c97a038f470ad154669
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-16 11:40:41 -0300

    feat: cria closeButton

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  { Popover } from '@headlessui/react';
+import './CloseButton.css';
+import { X } from 'phosphor-react';
+
+export default function CloseButton() {
+  return (
+    <Popover.Button
+      className="cl-btn"
+      title="Fechar formulário de feedback"
+    >
+      <X weight="bold" className="x-btn" />
+    </Popover.Button>
+  )
+}
